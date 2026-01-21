384c059342cb94aa67301d1f23286d305dfea574
Driver Station <driverstation1714@gmail.com>
2026-01-20 18:28:29 -0600

    auto aim started

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,195 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.pathplanner.lib.auto.NamedCommands;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.swervedrive.drivebase.AbsoluteFieldDrive;
+import frc.robot.subsystems.swervedrive.Eyes;
+import frc.robot.subsystems.swervedrive.SwerveSubsystem;
+import java.io.File;
+import swervelib.SwerveInputStream;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a "declarative" paradigm, very
+ * little robot logic should actually be handled in the {@link Robot} periodic methods (other than the scheduler calls).
+ * Instead, the structure of the robot (including subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer
+{
+  
+
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  final         CommandPS5Controller driver = new CommandPS5Controller(0);
+  // The robot's subsystems and commands are defined here...
+  private final SwerveSubsystem       drivebase  = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
+                                                                                "swerve/falcon"));
+  
+  public final Eyes s_Eyes = new Eyes(drivebase);
+
+  /**
+   * Converts driver input into a field-relative ChassisSpeeds that is controlled by angular velocity.
+   */
+  SwerveInputStream driveAngularVelocity = SwerveInputStream.of(drivebase.getSwerveDrive(),
+                                                                () -> driver.getLeftY() * -1,
+                                                                () -> driver.getLeftX() * -1)
+                                                            .withControllerRotationAxis(() -> s_Eyes.getTargetRotation())
+                                                            .deadband(OperatorConstants.DEADBAND)
+                                                            .scaleTranslation(0.8)
+                                                            .allianceRelativeControl(true);
+
+  /**
+   * Clone's the angular velocity input stream and converts it to a fieldRelative input stream.
+   */
+  SwerveInputStream driveDirectAngle = driveAngularVelocity.copy().withControllerHeadingAxis(driver::getRightX,
+                                                                                             driver::getRightY)
+                                                           .headingWhile(true);
+
+  /**
+   * Clone's the angular velocity input stream and converts it to a robotRelative input stream.
+   */
+  SwerveInputStream driveRobotOriented = driveAngularVelocity.copy().robotRelative(true)
+                                                             .allianceRelativeControl(false);
+
+  SwerveInputStream driveAngularVelocityKeyboard = SwerveInputStream.of(drivebase.getSwerveDrive(),
+                                                                        () -> -driver.getLeftY(),
+                                                                        () -> -driver.getLeftX())
+                                                                    .withControllerRotationAxis(() -> driver.getRawAxis(
+                                                                        2))
+                                                                    .deadband(OperatorConstants.DEADBAND)
+                                                                    .scaleTranslation(0.8)
+                                                                    .allianceRelativeControl(true);
+  // Derive the heading axis with math!
+  SwerveInputStream driveDirectAngleKeyboard     = driveAngularVelocityKeyboard.copy()
+                                                                               .withControllerHeadingAxis(() ->
+                                                                                                              Math.sin(
+                                                                                                                  driver.getRawAxis(
+                                                                                                                      2) *
+                                                                                                                  Math.PI) *
+                                                                                                              (Math.PI *
+                                                                                                               2),
+                                                                                                          () ->
+                                                                                                              Math.cos(
+                                                                                                                  driver.getRawAxis(
+                                                                                                                      2) *
+                                                                                                                  Math.PI) *
+                                                                                                              (Math.PI *
+                                                                                                               2))
+                                                                               .headingWhile(true)
+                                                                               .translationHeadingOffset(true)
+                                                                               .translationHeadingOffset(Rotation2d.fromDegrees(
+                                                                                   0));
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer()
+  {
+
+    Command driveFieldOrientedDirectAngle = drivebase.driveCommand(
+      () -> driver.getLeftY(),
+      () -> driver.getLeftX(),
+      () -> s_Eyes.getTargetRotation()
+      );
+
+      drivebase.setDefaultCommand(driveFieldOrientedDirectAngle);
+
+    // Configure the trigger bindings
+    configureBindings();
+    DriverStation.silenceJoystickConnectionWarning(true);
+    NamedCommands.registerCommand("test", Commands.print("I EXIST"));
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary predicate, or via the
+   * named factories in {@link edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller PS4}
+   * controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight joysticks}.
+   */
+  private void configureBindings()
+  {
+    Command driveFieldOrientedDirectAngle      = drivebase.driveFieldOriented(driveDirectAngle);
+    Command driveFieldOrientedAnglularVelocity = drivebase.driveFieldOriented(driveAngularVelocity);
+    Command driveRobotOrientedAngularVelocity  = drivebase.driveFieldOriented(driveRobotOriented);
+    Command driveSetpointGen = drivebase.driveWithSetpointGeneratorFieldRelative(
+        driveDirectAngle);
+    Command driveFieldOrientedDirectAngleKeyboard      = drivebase.driveFieldOriented(driveDirectAngleKeyboard);
+    Command driveFieldOrientedAnglularVelocityKeyboard = drivebase.driveFieldOriented(driveAngularVelocityKeyboard);
+    Command driveSetpointGenKeyboard = drivebase.driveWithSetpointGeneratorFieldRelative(
+        driveDirectAngleKeyboard);
+
+    if (RobotBase.isSimulation())
+    {
+      drivebase.setDefaultCommand(driveFieldOrientedDirectAngleKeyboard);
+    } else
+    {
+      drivebase.setDefaultCommand(driveFieldOrientedAnglularVelocity);
+    }
+
+    if (Robot.isSimulation())
+    {
+      Pose2d target = new Pose2d(new Translation2d(1, 4),
+                                 Rotation2d.fromDegrees(90));
+      //drivebase.getSwerveDrive().field.getObject("targetPose").setPose(target);
+      driveDirectAngleKeyboard.driveToPose(() -> target,
+                                           new ProfiledPIDController(5,
+                                                                     0,
+                                                                     0,
+                                                                     new Constraints(5, 2)),
+                                           new ProfiledPIDController(5,
+                                                                     0,
+                                                                     0,
+                                                                     new Constraints(Units.degreesToRadians(360),
+                                                                                     Units.degreesToRadians(180))
+                                           ));
+      driver.triangle().onTrue(Commands.runOnce(() -> drivebase.resetOdometry(new Pose2d(3, 3, new Rotation2d()))));
+      // driver.button(1).whileTrue(drivebase.sysIdDriveMotorCommand());
+      // driver.button(2).whileTrue(Commands.runEnd(() -> driveDirectAngleKeyboard.driveToPoseEnabled(true),
+                                                    //  () -> driveDirectAngleKeyboard.driveToPoseEnabled(false)));
+
+      // driver.L2().whileTrue(
+      //    driveFieldOrientedDirectAngle = drivebase.driveCommand(
+      //     () -> driver.getLeftY(),
+      //     () -> driver.getLeftX(),
+      //     () -> s_Eyes.getTargetRotation()).andThen(() ->System.out.println("Worked"))
+
+      // );
+
+    }
+   
+
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand()
+  {
+    // An example command will be run in autonomous
+    return drivebase.getAutonomousCommand("New Auto");
+  }
+
+  public void setMotorBrake(boolean brake)
+  {
+    drivebase.setMotorBrake(brake);
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/swervedrive/Eyes.java
@@ -0,0 +1,265 @@
+/*
+ * This subsytem controls robot vision. It uses the LimelightHelpers class
+ * and contains methods that are used to track april tags and gather
+ * positional data from them. This data is then used in the swerve
+ * subsystem to update the robot's odometry using the pose estimator.
+ * 
+ * parameters:
+ * none
+ */
+
+
+ package frc.robot.subsystems.swervedrive;
+
+
+ import frc.robot.Constants;
+ import java.util.List;
+ 
+ import com.pathplanner.lib.path.GoalEndState;
+ import com.pathplanner.lib.path.IdealStartingState;
+ import com.pathplanner.lib.path.PathConstraints;
+ import com.pathplanner.lib.path.PathPlannerPath;
+ import com.pathplanner.lib.path.Waypoint;
+ 
+ import edu.wpi.first.wpilibj.DriverStation;
+ import edu.wpi.first.wpilibj.DriverStation.Alliance;
+ import edu.wpi.first.wpilibj.Timer;
+ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+ import edu.wpi.first.wpilibj2.command.SubsystemBase;
+ import edu.wpi.first.math.VecBuilder;
+ import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+ import edu.wpi.first.math.geometry.Pose2d;
+ import edu.wpi.first.math.geometry.Pose3d;
+ import edu.wpi.first.math.geometry.Rotation2d;
+ import edu.wpi.first.math.geometry.Rotation3d;
+ import edu.wpi.first.math.geometry.Translation2d;
+ import edu.wpi.first.math.util.Units;
+ import edu.wpi.first.networktables.NetworkTable;
+ import edu.wpi.first.networktables.NetworkTableEntry;
+ import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.PubSubOption;
+import edu.wpi.first.networktables.StructPublisher;
+ import frc.robot.subsystems.swervedrive.*;
+import limelight.Limelight;
+ 
+ 
+ 
+ public class Eyes extends SubsystemBase {
+ 
+     // Swerve subsystem for pose estimator
+     SwerveSubsystem s_Swerve;
+     LimelightHelpers r_LimelightHelpers;
+
+     public StructPublisher<Pose2d> estimatedRobotPosePublisher;
+     public SwerveDrivePoseEstimator m_PoseEstimator;
+ 
+     // create objects and variables
+     public LimelightHelpers limelight;
+     public double tx;
+     public double ty;
+     public double ta;
+     public double tID;
+     public double txnc;
+     public double tync;
+ 
+     public int closestReefSide;
+ 
+ 
+    
+     public boolean controllerRumble = false;
+     public PathPlannerPath reefPath;
+     public boolean closeToReef = false;
+     public double closestDistance;
+     // constuctor
+     public Eyes(SwerveSubsystem swerve) {
+ 
+         s_Swerve = swerve;
+         // reefPath = closestReefpath(-1);
+
+
+    estimatedRobotPosePublisher = NetworkTableInstance.getDefault().getStructTopic("/EstimatedRobotPose", Pose2d.struct).publish(PubSubOption.sendAll(true));//publisher needs arguments
+    m_PoseEstimator = 
+    new SwerveDrivePoseEstimator(
+        s_Swerve.getKinematics(),
+        s_Swerve.getHeading(),
+        s_Swerve.getSwerveDrive().getModulePositions(),
+        new Pose2d(),
+        VecBuilder.fill(0.1, 0.1, 0.1),
+        VecBuilder.fill(1.5, 1.5, 1.5));
+        
+         
+     }
+ 
+  
+     /*
+      * This method will gather all of the positional data of the limelight target.
+      * 
+      * parameters:
+      * none
+      * 
+      * returns;
+      * none
+      */
+     public void updateData() {
+ 
+         /* 
+          * get data from limelight target
+          * tx = x position in degrees in limelight FOV
+          * ty = y position in degrees in limelight FOV
+          * ta = pitch in degrees in limelight FOV
+          * tID = target ID number
+          */
+         NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight-front");
+         NetworkTableEntry tx = table.getEntry("tx");
+         NetworkTableEntry ty = table.getEntry("ty");
+         NetworkTableEntry ta = table.getEntry("ta");
+ 
+         //read values periodically
+         double x = tx.getDouble(tID);
+         double y = ty.getDouble(0.0);
+         double area = ta.getDouble(0.0);
+ 
+         //post to smart dashboard periodically
+         SmartDashboard.putNumber("LimelightX", x);
+         SmartDashboard.putNumber("LimelightY", y);
+         SmartDashboard.putNumber("LimelightArea", area);
+ 
+         // tx = LimelightHelpers.getTX("limelight");
+         // ty = LimelightHelpers.getTY("limelight");
+         // ta = LimelightHelpers.getTA("limelight");
+         //   tID = LimelightHelpers.getFiducialID("limelight");
+ 
+         // txnc = LimelightHelpers.getTXNC("limelight");  // Horizontal offset from principal pixel/point to target in degrees
+         // tync = LimelightHelpers.getTYNC("limelight");  // Vertical  offset from principal pixel/point to target in degrees
+ 
+         LimelightHelpers.setPipelineIndex("limelight-front", 0);
+ 
+         // log target data
+         SmartDashboard.putNumber("AprilTagID", tID);
+
+
+ 
+     }
+ 
+     /*
+      * This method will wrap all target data into an array for easy access.
+      * 
+      * Array indexes:
+      * [0] = x
+      * [1] = y
+      * [2] = a (pitch)
+      * [3] = ID
+      */
+     public double[] getDataPackage() {
+ 
+         double[] data = {
+             tx,
+             ty,
+             ta,
+             tID
+         };
+ 
+         return data;
+     }
+ 
+     /*
+      * This method will return the pose of the robot based upon the pose of a detected apriltag
+      * 
+      * parameters:
+      * none
+      * 
+      * returns:
+      * robot pose      (Pose2d)
+      */
+     public Pose2d getRobotPose() {
+ 
+         Pose2d pose;
+ 
+         
+         pose = LimelightHelpers.getBotPose2d_wpiBlue("limelight-front");
+         return pose;
+         
+         
+     }
+
+     public double getTargetRotation() {
+        Pose2d robotPose = m_PoseEstimator.getEstimatedPosition();
+        Pose3d targetPose = getTargetPose();
+
+        double robotX = robotPose.getX();
+        double robotY = robotPose.getY();
+
+        double targetX = targetPose.getX();
+        double targetY = targetPose.getY();
+
+        double angle =  (Math.atan((targetY - robotY) / (targetX - robotX)) * (180 / Math.PI));
+
+        if (robotX > targetX) {
+
+            angle = angle + 180;
+
+        }
+
+        SmartDashboard.putNumber("angle", angle);
+        SmartDashboard.putNumber(" inverted angle", -angle);
+
+        return -angle + 180;
+     }
+
+     public Pose3d getTargetPose() {
+        Pose3d pose;
+
+
+        if(DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Blue) {
+
+            // get pose of blue speaker
+            pose = new Pose3d(Constants.Positions.hubBlueX, Constants.Positions.hubBlueY, 0, new Rotation3d(0,0,Constants.Positions.hubBlueR));
+
+        // if robot is on red alliance
+        } else {
+
+            // get pose of red speaker
+            pose = new Pose3d(Constants.Positions.hubBlueX, Constants.Positions.hubBlueY, 0, new Rotation3d(0,0,Constants.Positions.hubBlueY));
+
+        }
+        
+        return pose;
+     }
+     
+ 
+     /*
+      * This method will return the known pose of the desired target.
+      * 
+      * parameters:
+      * none
+      * 
+      * returns:
+      * target pose      (Pose2d)
+      */
+     
+ 
+     @Override
+     public void periodic() {
+
+        m_PoseEstimator.update(s_Swerve.getHeading(), s_Swerve.getSwerveDrive().getModulePositions());
+        estimatedRobotPosePublisher.set(m_PoseEstimator.getEstimatedPosition());
+
+        //  if (LimelightHelpers.getTV("limelight-front") == true) {
+        //      s_Swerve.getSwerveDrive().addVisionMeasurement(
+        //          getRobotPose(), 
+        //          Timer.getFPGATimestamp() - (LimelightHelpers.getLatency_Pipeline("limelight-front")/1000.0) - (LimelightHelpers.getLatency_Capture("limelight")/1000.0)
+        //      );
+        //  }
+        m_PoseEstimator.addVisionMeasurement( getRobotPose(),
+             Timer.getFPGATimestamp() - (LimelightHelpers.getLatency_Pipeline("limelight-front")/1000.0) - (LimelightHelpers.getLatency_Capture("limelight")/1000.0)
+);
+ 
+         updateData();
+ 
+        
+ 
+ 
+        
+ 
+     }
+ }
