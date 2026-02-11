2467d0dbf4b55e8f878d0a40cad8182d4159b684
Driver Station <driverstation1714@gmail.com>
2026-02-10 19:56:14 -0600

    Reapply "git problem"
    This reverts commit ec0f8eaf234269a4b423dc1efe1435ec4db2fcc5.

---
+++ src/main/java/frc/robot/subsystems/HotDog.java
@@ -0,0 +1,138 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Pounds;
+import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.Seconds;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.hardware.TalonFXS;
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import yams.gearing.GearBox;
+import yams.gearing.MechanismGearing;
+import yams.mechanisms.SmartMechanism;
+import yams.mechanisms.config.FlyWheelConfig;
+import yams.mechanisms.velocity.FlyWheel;
+import yams.motorcontrollers.SmartMotorController;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+import yams.motorcontrollers.local.SparkWrapper;
+import yams.motorcontrollers.remote.TalonFXSWrapper;
+
+
+public class HotDog extends SubsystemBase {
+
+  private double indexerP = .1;
+  private double indexerI = 0;
+  private double indexerD = 0;
+  private double hotDogP = .1;
+  private double hotDogI = 0;
+  private double hotDogD = 0;
+  
+  private double currentLimit = 70;
+  private double indexerSpeed = 20;
+  private double hotDogSpeed = 20;
+  private double reverseHotDogSpeed = -2;
+  private double reverseIndexerSpeed = -2;
+
+  private int indexerID = 16;
+  private int hotDogID = 0;
+  private double gearRatio = 0;
+
+  private TalonFX m_Indexer;
+  private TalonFX m_HotDog;
+  private TalonFXConfiguration indexerConfigs;
+  private TalonFXConfiguration hotDogConfigs;
+  private VelocityVoltage m_velocityRequest;
+
+
+  
+
+  /** Creates a new Shooter. */
+  public HotDog() {
+    m_Indexer = new TalonFX(indexerID);
+    m_HotDog = new TalonFX(hotDogID);
+    m_velocityRequest = new VelocityVoltage(0).withSlot(0);
+
+    indexerConfigs = new TalonFXConfiguration();
+    indexerConfigs.Slot0.kP = indexerP;
+    indexerConfigs.Slot0.kI = indexerI;
+    indexerConfigs.Slot0.kD = indexerD;
+    indexerConfigs.CurrentLimits.SupplyCurrentLimitEnable = true;
+    indexerConfigs.CurrentLimits.SupplyCurrentLimit = currentLimit;
+
+    hotDogConfigs = new TalonFXConfiguration();
+    hotDogConfigs.Slot0.kP = hotDogP;
+    hotDogConfigs.Slot0.kI = hotDogI;
+    hotDogConfigs.Slot0.kD = hotDogD;
+    hotDogConfigs.CurrentLimits.SupplyCurrentLimitEnable = true;
+    hotDogConfigs.CurrentLimits.SupplyCurrentLimit = currentLimit;
+
+    m_Indexer.getConfigurator().apply(indexerConfigs);
+    m_HotDog.getConfigurator().apply(hotDogConfigs);
+
+
+  }
+
+  // public void setIndexerSpeed(double setpoint)
+  // {
+  //   m_Indexer.setControl(m_velocityRequest.withVelocity(setpoint));
+  // }
+
+  // public double getIndexerSpeed()
+  // {
+  //   return indexerSpeed;
+  // }
+
+  // public double getReverseIndexerSpeed()
+  // {
+  //   return reverseIndexerSpeed;
+  // }
+
+  public void setHotDogSpeed(double setpoint)
+  {
+    m_HotDog.setControl(m_velocityRequest.withVelocity(setpoint));
+    m_Indexer.setControl(m_velocityRequest.withVelocity(setpoint));
+  }
+
+  public double getHotDogSpeed()
+  {
+    return hotDogSpeed;
+  }
+
+  public double getReverseHotDogSpeed()
+  {
+    return reverseHotDogSpeed;
+  }
+
+
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    // shooter.updateTelemetry();
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+    // shooter.simIterate();
+  }
+}
